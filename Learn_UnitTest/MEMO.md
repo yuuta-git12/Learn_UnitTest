52cdbdc19c3e1316994938340402685cc90d80ff
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-02-14 00:02:23 +0900

    [add]メモとテストコード追加

---
+++ Learn_UnitTest/MEMO.md
@@ -0,0 +1,8 @@
+﻿
+- このプロジェクトは「単体テストの考え方/使い方」のサンプルコードを動かして確認するために使用する。
+- Visual Studioで単体テストのプロジェクトを作成する場合はテンプレートの「MS Testプロジェクト」を選択する。
+- サンプルコードにあるAssertオブジェクトを使用するには「using Microsoft.VisualStudio.TestTools.UnitTesting;」の宣言が必要
+
+# 参考サイト
+- [【C#】ユニットテストで使用するAssertの基本を解説](https://genesis-tech.jp/blog/basics-of-assert/)
+- [MsTestによるユニットテストの解説](https://qiita.com/mima_ita/items/55394bcc851eb8b6dc24)
---
+++ Learn_UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace Learn_UnitTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        bool result = IsStringLong("abcddddd");
+        //AreEqualメソッド：二つのオブジェクトを比較して同じであるかを判定する。
+        Assert.AreEqual(false, result);
+    }
+
+    public static bool IsStringLong(string input)
+    {
+        if (input.Length > 5)
+            return true;
+        return false;
+    }
+}
